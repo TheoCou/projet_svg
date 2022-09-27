b8c93d0f6cfdcc298fc0d7b869d2b5ba718c0cdb
TheoCou <theocourson@gmail.com>
2022-09-27 19:12:00 +0200

    new file:   asset/cross.svg 	new file:   asset/minus.svg 	new file:   css/style.css 	new file:   index.html 	new file:   js/app.js

---
+++ js/app.js
@@ -0,0 +1,51 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  var y = document.getElementById("myIMG");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.src = "asset/minus.svg";
+  } else {
+    x.style.display = "none";
+    y.src = "asset/cross.svg";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  var y = document.getElementById("myIMG2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.src = "asset/minus.svg";
+  } else {
+    x.style.display = "none";
+    y.src = "asset/cross.svg";
+  }
+}
+
+
+function myFunction3() {
+  var x = document.getElementById("myDIV3");
+  var y = document.getElementById("myIMG3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.src = "asset/minus.svg";
+  } else {
+    x.style.display = "none";
+    y.src = "asset/cross.svg";
+  }
+}
+
+
+function myFunction4() {
+  var x = document.getElementById("myDIV4");
+  var y = document.getElementById("myIMG4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.src = "asset/minus.svg";
+  } else {
+    x.style.display = "none";
+    y.src = "asset/cross.svg";
+  }
+}
+
+
